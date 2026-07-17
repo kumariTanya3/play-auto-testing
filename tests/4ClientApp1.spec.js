4aae2f125351a7550bd95e882fbfa69d1c3b1929
kumariTanya3 <tanya.edo3@gmail.com>
2026-07-17 16:31:13 +0530

    End to end Application automation

--- tests/4ClientApp1.spec.js
+++ tests/4ClientApp1.spec.js
@@ -10,7 +10,7 @@ test.only('Clent App', async ({page}) =>
   await page.waitForLoadState('networkidle'); 
   await page.locator(".card-body b").first().waitFor(); //6. if images and other details take time to load
   const titles =await page.locator(".card-body b").allTextContents();
-  console.log(titles);
+  //console.log(titles);
   const c = await products.count();
   for(let i = 0; i < c; ++i)
   {
@@ -26,7 +26,33 @@ test.only('Clent App', async ({page}) =>
   //6. [routerlink*=cart], where
   //'routerlink' is an attribute
   //'*' is used for partial text
-  const b = page.locator("h3:has-text('car')").isVisible();
-  //':has-text' is known as psuedo-class;
-  expect(b).toBeTruthy();
+  
+  //page.locator("div li").waitFor(); //9. error, as waiting for complete page to load, result in this line resulting mutliple elemts on the same page, so if one item is loaded, we will proceed to check for 'car' as in '7.'
+  page.locator("div li").first().waitFor(); //8. we are tracking the exact attribute that has our product-name.
+  //this statement 'first().waitfor()' helps in waiting for page to load, even if one element is in page. as using 'waitfor()' simply returned error 
+  //const b = await page.locator("h3:has-text('car')").isVisible();
+  //7. ':has-text' is known as psuedo-class;
+  //expect(b).toBeTruthy();
+  //line 33 and 34 causing error
+  const b = await page.locator("h3", {hasText: 'car'});
+  await expect(b).toBeVisible();
+  await page.locator("text=Checkout").click();
+  //10. or we can use attribute, button to locate.
+  await page.locator("[placeholder*='Country']").pressSequentially("ind", {delay: 200}); //200ms
+  const dropdown = page.locator(".ta-results");
+  //11. '.ta-results' is the section of options available while typing country name. 
+  await dropdown.waitFor();
+  const numoptn = await dropdown.locator("button").count(); //12. number-of-options counting;
+  for(let i = 0; i < numoptn; ++i)
+  {
+    const text = await dropdown.locator("button").nth(i).textContent(); //13. each text in the drop down as we enter the string 'ind' sequentially;
+    if(text === " India") //14. this text is written with space and 'I' because, while inspecting the web-element the word 'india' is written like that. and if the text in playwright doesn't match, it will return error while executing the automation. because this will retrieve the text directly for matching the text.   
+    //or we can use trimming or other method to make task easier like:
+    //if(text.includes("India")) //this also works //this may cause some error if other words like india is present... so better use trim, or use this 'includes' statement is no dupicate word like 'india' is in search element. here 'british indian ocean territory' is present. so better stick to first method, or trim word.
+    {
+      await dropdown.locator("button").nth(i).click; 
+      break;
+    }
+  }
+  await page.pause();
 });
