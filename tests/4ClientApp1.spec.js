4051466b5b63df762c414b8a81665ae9c1aa1d31
kumariTanya3 <tanya.edo3@gmail.com>
2026-07-22 00:47:58 +0530

    fix the select country

--- tests/4ClientApp1.spec.js
+++ tests/4ClientApp1.spec.js
@@ -1,11 +1,13 @@
 const {test, expect} = require('@playwright/test')
 test.only('Clent App', async ({page}) =>  
 {
-  const pronm = 'car'; //2. product-name
+
   const products = page.locator(".card-body"); //1. this is like array that has all products
   await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
   await page.locator("#userEmail").fill('anshika@gmail.com');
+  const email = "anshika@gmail.com";
   await page.locator("#userPassword").fill('Iamking@000');
+  const pronm = 'car'; //2. product-name
   await page.locator("[value='Login']").click();
   await page.waitForLoadState('networkidle'); 
   await page.locator(".card-body b").first().waitFor(); //6. if images and other details take time to load
@@ -38,21 +40,27 @@ test.only('Clent App', async ({page}) =>
   await expect(b).toBeVisible();
   await page.locator("text=Checkout").click();
   //10. or we can use attribute, button to locate.
-  await page.locator("[placeholder*='Country']").pressSequentially("ind", {delay: 200}); //200ms
-  const dropdown = page.locator(".ta-results");
-  //11. '.ta-results' is the section of options available while typing country name. 
+  await page.locator("[placeholder*='Select Country']").pressSequentially("ind", {delay: 100}); //100ms
+  const dropdown = page.locator(".ta-results", {hasText: /^India$/i});
+  //11. '.ta-results' is the section of options available while typing country name. the '/^India$/i' ensures only 'india' is getting selected.
   await dropdown.waitFor();
+  await dropdown.click();
   const numoptn = await dropdown.locator("button").count(); //12. number-of-options counting;
   for(let i = 0; i < numoptn; ++i)
   {
     const text = await dropdown.locator("button").nth(i).textContent(); //13. each text in the drop down as we enter the string 'ind' sequentially;
-    if(text === " India") //14. this text is written with space and 'I' because, while inspecting the web-element the word 'india' is written like that. and if the text in playwright doesn't match, it will return error while executing the automation. because this will retrieve the text directly for matching the text.   
+    if(text.trim() === " India") //14. this text is written with space and 'I' because, while inspecting the web-element the word 'india' is written like that. and if the text in playwright doesn't match, it will return error while executing the automation. because this will retrieve the text directly for matching the text.
     //or we can use trimming or other method to make task easier like:
     //if(text.includes("India")) //this also works //this may cause some error if other words like india is present... so better use trim, or use this 'includes' statement is no dupicate word like 'india' is in search element. here 'british indian ocean territory' is present. so better stick to first method, or trim word.
     {
-      await dropdown.locator("button").nth(i).click; 
+      await dropdown.locator("button").nth(i).click();
       break;
     }
   }
+  await expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+
+  //15. 'user__name mt-5' is classname, these are two class name via which we can access the parent class for the email box. (the box has two email areas, one is placeholder, other is user input. so to grab unique location, go to parent class first, so its easy get unique locator for required mail). //it was still not unique. 2 elemts were there, so we return the 'first' one
+  await page.locator(".action__submit").click(); //16. 'btnn action__submit ng-star-inserted' the classname of submit btn. as 'action__submit' is unique in the page. so we can proceed without any issue.
+  //await expect(page.locator())
   await page.pause();
 });
